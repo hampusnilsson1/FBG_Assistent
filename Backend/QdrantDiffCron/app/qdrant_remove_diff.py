0b199dc624af56c05d06ad5ec6a096d338ea6857
hampusnilsson1 <148866226+hampusnilsson1@users.noreply.github.com>
2025-05-19 11:15:20 +0200

    Email Alert Implemented
    Email alerts are now sent out if there are more than 50 urls difference in qdrant and sitemap. To prevent massive removals and bugs wiping the database.

--- Backend/QdrantDiffCron/app/qdrant_remove_diff.py
+++ Backend/QdrantDiffCron/app/qdrant_remove_diff.py
@@ -6,9 +6,19 @@ from dotenv import load_dotenv
 from qdrant_client import QdrantClient
 from qdrant_client.http import models
 
+import smtplib
+from email.message import EmailMessage
+
 # Ladda API-nyckel från .env-fil
 load_dotenv(dotenv_path="/app/data/API_KEYS.env")
 qdrant_api_key = os.getenv("QDRANT_API_KEY")
+
+load_dotenv(dotenv_path="/app/data/EMAIL_INFO.env")
+EMAIL_USER = os.getenv("EMAIL_USER")
+EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
+EMAIL_TO = os.getenv("EMAIL_TO")
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
 
 QDRANT_URL = "https://qdrant.utvecklingfalkenberg.se/"
 QDRANT_PORT = 443
@@ -17,6 +27,7 @@ COLLECTION_NAME = "FalkenbergsKommunsHemsida"
 qdrant_client = QdrantClient(
     url=QDRANT_URL, port=QDRANT_PORT, https=True, api_key=qdrant_api_key
 )
+
 
 ## Web Sitemap Process Remove Diff
 # Hämta alla punkter från Qdrant
@@ -64,11 +75,11 @@ def get_web_qdrant_urls():
                 source_url = payload.get("source_url", None)
                 # Filter out empty URLs and PDF files
                 if not url or "evolution" in url:
-                    #print("Punkt saknar URL eller är en Evolution PDF. Hoppar över.")
+                    # print("Punkt saknar URL eller är en Evolution PDF. Hoppar över.")
                     continue
                 # Filter out Linked Documents
                 if source_url:
-                    #print("Punkt är ett länkat dokument. Hoppar över.")
+                    # print("Punkt är ett länkat dokument. Hoppar över.")
                     continue
 
                 results.append(url)
@@ -82,6 +93,7 @@ def get_web_qdrant_urls():
             break
 
     return set(results)
+
 
 # Hämta URL:er från sitemap
 def get_web_sitemap_urls(sitemap_url):
@@ -98,7 +110,8 @@ def get_web_sitemap_urls(sitemap_url):
     else:
         raise Exception(f"Failed to fetch sitemap from {sitemap_url}")
 
-def remove_web_sitemap_url_diff():
+
+def remove_web_sitemap_url_diff(force=False):
     sitemap_url = (
         "https://kommun.falkenberg.se/index.php?option=com_jmap&view=sitemap&format=xml"
     )
@@ -109,8 +122,14 @@ def remove_web_sitemap_url_diff():
 
         # Hitta URL:er som finns i Qdrant men inte i sitemap
         missing_urls = qdrant_urls - sitemap_urls
-        print( f"Totalt {len(missing_urls)} URL:er saknas i sitemap.")
+        print(f"Totalt {len(missing_urls)} URL:er saknas i sitemap.")
         if missing_urls:
+            if len(missing_urls) > 50 and not force:
+                print(
+                    "För många URL:er skiljer sig från sitemap, vänligen kontrollera."
+                )
+                send_alert_email(len(missing_urls), "Webb")
+                return
             print("URL:er som finns i Qdrant men inte i sitemap:")
             for url in missing_urls:
                 print(url)
@@ -118,12 +137,12 @@ def remove_web_sitemap_url_diff():
             remove_qdrant_urls(missing_urls)
         else:
             print("Alla Webb URL:er i Qdrant finns också i sitemap.")
-            
+
     except Exception as e:
         print(f"Ett fel inträffade: {e}")
-    
-    
-#Evolution Process Remove Diff
+
+
+# Evolution Process Remove Diff
 def get_evo_qdrant_urls():
     # Hämta alla evolution pdfer i databasen
     qdrant_filter = models.Filter(
@@ -155,8 +174,9 @@ def get_evo_qdrant_urls():
             if not version:
                 version = "0.1"
             qdrant_pdfs.append({"url": url, "version": version})
-            
+
     return qdrant_pdfs
+
 
 def get_evo_sitemap_urls(sitemap_url):
     response = requests.get(sitemap_url)
@@ -173,33 +193,34 @@ def get_evo_sitemap_urls(sitemap_url):
                 evolution_pdfs.append({"url": url, "version": version, "title": title})
         return evolution_pdfs
 
-def remove_evo_sitemap_url_diff():
+
+def remove_evo_sitemap_url_diff(force=False):
     qdrant_pdfs = get_evo_qdrant_urls()
-    evolution_pdfs = get_evo_sitemap_urls("https://intranet.falkenberg.se/fbg_apps/services/evolution/documents.php")
-    
+    evolution_pdfs = get_evo_sitemap_urls(
+        "https://intranet.falkenberg.se/fbg_apps/services/evolution/documents.php"
+    )
+
     qdrant_evolution_urls = set(pdf["url"] for pdf in qdrant_pdfs)
     sitemap_evolution_urls = set(pdf["url"] for pdf in evolution_pdfs)
-    
+
     # Find URLs to remove
     urls_to_remove = qdrant_evolution_urls - sitemap_evolution_urls
-    print(
-        f"Tar bort {len(urls_to_remove)} stycken gamla evolution pdfer."
-    )
+    if len(urls_to_remove) > 50 and not force:
+        print(
+            "För många evolution pdfer skiljer sig från sitemap, vänligen kontrollera."
+        )  # Byt till True för att tvinga borttagning
+        send_alert_email(len(urls_to_remove), "Evolution")
+        return
+    print(f"Tar bort {len(urls_to_remove)} stycken gamla evolution pdfer.")
     qdrant_filter = models.Filter(
         must=[
-            models.FieldCondition(
-                key="url", match=models.MatchAny(any=urls_to_remove)
-            ),
+            models.FieldCondition(key="url", match=models.MatchAny(any=urls_to_remove)),
         ]
     )
     points_selector = models.FilterSelector(filter=qdrant_filter)
     qdrant_client.delete(
         collection_name=COLLECTION_NAME, points_selector=points_selector
     )
-    
-    
-
-
 
 
 def remove_qdrant_urls(urls):
@@ -217,12 +238,30 @@ def remove_qdrant_urls(urls):
     )
 
 
+def send_alert_email(count, urltype="Webb"):
+    msg = EmailMessage()
+    msg.set_content(
+        f"Antalet URL:er för {urltype} har överskridit 50 (nuvarande antal: {count}). Inga gamla datapunkter har raderats från Qdrant. Du måste hantera detta manuellt."
+    )
+    msg["Subject"] = "Varning: För många URL:er - Falkis Backend"
+    msg["From"] = EMAIL_USER
+    msg["To"] = EMAIL_TO
+
+    try:
+        with smtplib.SMTP_SSL(EMAIL_HOST, EMAIL_PORT) as smtp:
+            smtp.login(EMAIL_USER, EMAIL_PASSWORD)
+            smtp.send_message(msg)
+            print("Mail skickat.")
+    except Exception as e:
+        print("Kunde inte skicka mail:", e)
+
+
 # Huvudfunktion för att jämföra URL:er
 def main():
     print("Tar bort gamla Webbsitemap URL:er...")
-    remove_web_sitemap_url_diff()
+    remove_web_sitemap_url_diff(force=False)  # True för att tvinga borttagning
     print("Tar bort gamla Evolution URL:er...")
-    remove_evo_sitemap_url_diff()
+    remove_evo_sitemap_url_diff(force=False)  # True för att tvinga borttagning
 
 
 if __name__ == "__main__":
